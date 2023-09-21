48bfa91a90481a6fec0a69e515a4b1494e3ccdae
Roland Lötscher <roland_loetscher@hotmail.com>
2023-09-21 13:51:06 +0200

    Toggle Dark/Light theme

--- preferences/main.js
+++ preferences/main.js
@@ -1,3 +1,6 @@
+import Adw from "gi://Adw";
+
+const style_manager = Adw.StyleManager.get_default();
 const pref_window = workbench.builder.get_object("win_prefs");
 const row_generation_info = workbench.builder.get_object("row_generation_info");
 const info_latex_generation = workbench.builder.get_object(
@@ -6,6 +9,18 @@ const info_latex_generation = workbench.builder.get_object(
 const info_latex_generation_back = workbench.builder.get_object(
   "info_latex_generation_back",
 );
+
+sw_dark_theme.active = style_manager.dark;
+
+sw_dark_theme.connect("notify::active", () => {
+  console.log("Connected");
+  // When the Switch is toggled, set the color scheme
+  if (sw_dark_theme.active) {
+    style_manager.color_scheme = Adw.ColorScheme.FORCE_DARK;
+  } else {
+    style_manager.color_scheme = Adw.ColorScheme.FORCE_LIGHT;
+  }
+});
 
 row_generation_info.connect("activated", () => {
   pref_window.present_subpage(info_latex_generation);
