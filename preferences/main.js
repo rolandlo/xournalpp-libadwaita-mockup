8ddc9c5988f4c548a715da1eb5b98ed19c9b731d
Roland Lötscher <roland_loetscher@hotmail.com>
2023-09-21 13:52:28 +0200

    Make FileDialog pop up and set file name

--- preferences/main.js
+++ preferences/main.js
@@ -1,4 +1,8 @@
+import Gtk from "gi://Gtk";
+import Gio from "gi://Gio";
 import Adw from "gi://Adw";
+
+Gio._promisify(Gtk.FileDialog.prototype, "open", "open_finish");
 
 const style_manager = Adw.StyleManager.get_default();
 const pref_window = workbench.builder.get_object("win_prefs");
@@ -9,6 +13,8 @@ const info_latex_generation = workbench.builder.get_object(
 const info_latex_generation_back = workbench.builder.get_object(
   "info_latex_generation_back",
 );
+const button_template = workbench.builder.get_object("btn_template");
+const sw_dark_theme = workbench.builder.get_object("sw_dark_theme");
 
 sw_dark_theme.active = style_manager.dark;
 
@@ -28,5 +34,21 @@ row_generation_info.connect("activated", () => {
 
 info_latex_generation_back.connect("clicked", () => {
   pref_window.close_subpage();
+});
+
+async function openFile() {
+  const dialog_for_file = new Gtk.FileDialog();
+  const file = await dialog_for_file.open(workbench.window, null);
+  const info = file.query_info(
+    "standard::name",
+    Gio.FileQueryInfoFlags.NONE,
+    null,
+  );
+  const button_content = button_template.get_child();
+  button_content.set_label(info.get_name());
+}
+
+button_template.connect("clicked", () => {
+  openFile().catch(console.error);
 });
 
