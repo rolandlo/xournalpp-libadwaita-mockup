d2c1d912266c45efb565de2e7e022292cf9e3e21
Roland Lötscher <roland_loetscher@hotmail.com>
2023-09-22 21:11:43 +0200

    Replace Tex subpage by ExpanderRow

--- preferences/main.js
+++ preferences/main.js
@@ -7,13 +7,6 @@ Gio._promisify(Gtk.FileDialog.prototype, "open", "open_finish");
 const btn_check = workbench.builder.get_object("btn_check");
 const style_manager = Adw.StyleManager.get_default();
 const pref_window = workbench.builder.get_object("win_prefs");
-const row_generation_info = workbench.builder.get_object("row_generation_info");
-const info_latex_generation = workbench.builder.get_object(
-  "info_latex_generation",
-);
-const info_latex_generation_back = workbench.builder.get_object(
-  "info_latex_generation_back",
-);
 const button_template = workbench.builder.get_object("btn_template");
 const sw_dark_theme = workbench.builder.get_object("sw_dark_theme");
 const lbl_placeholders = workbench.builder.get_object("placeholders");
@@ -56,14 +49,6 @@ sw_dark_theme.connect("notify::active", () => {
   } else {
     style_manager.color_scheme = Adw.ColorScheme.FORCE_LIGHT;
   }
-});
-
-row_generation_info.connect("activated", () => {
-  pref_window.present_subpage(info_latex_generation);
-});
-
-info_latex_generation_back.connect("clicked", () => {
-  pref_window.close_subpage();
 });
 
 async function openFile() {
