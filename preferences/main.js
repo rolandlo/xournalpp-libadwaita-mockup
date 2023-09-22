8350b3cdb38ccd5f58db6471facedeee69df72fc
Roland Lötscher <roland_loetscher@hotmail.com>
2023-09-22 16:30:23 +0200

    Add Defaults, Language, Load/Save preferences page

--- preferences/main.js
+++ preferences/main.js
@@ -16,6 +16,35 @@ const info_latex_generation_back = workbench.builder.get_object(
 );
 const button_template = workbench.builder.get_object("btn_template");
 const sw_dark_theme = workbench.builder.get_object("sw_dark_theme");
+const lbl_placeholders = workbench.builder.get_object("placeholders");
+
+lbl_placeholders.set_label(
+  `
+%a    Abbreviated weekday name (e.g. Thu)
+%A    Full weekday name (e.g. Thursday)
+%b    Abbreviated month name (e.g. Aug)
+%B    Full month name (e.g. August)
+%c    Date and time representation (e.g. Thu Aug 23 14:55:02 2001)
+%d    Day of the month (01-31)
+%F    Date representation (e.g. 2001-08-23)
+%H    Hour in 24h format (00-23)
+%I    Hour in 12h format (01-12)
+%j    Day of the year (001-366)
+%m    Month as a decimal number (01-12)
+%M    Minute (00-59)
+%p    AM or PM designation (e.g. PM)
+%S    Second (00-61)
+%U    Week number with the first Sunday as the first day of week one (00-53)
+%w    Weekday as a decimal number with Sunday as 0 (0-6)
+%W    Week number with the first Monday as the first day of week one (00-53)
+%x    Date representation (e.g. 08/23/01)
+%X    Time representation (e.g. 14:55:02)
+%y    Year, last two digits (00-99)
+%Y    Year (e.g. 2001)
+%Z    Timezone name or abbreviation (e.g. CDT)
+%%    A % sign
+`,
+);
 
 sw_dark_theme.active = style_manager.dark;
 
@@ -60,3 +89,4 @@ btn_check.connect("clicked", () => {
 
   pref_window.add_toast(toast);
 });
+
