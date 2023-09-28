d6088391d9105c8fc04d7277f7ac1a1f670193f0
Roland Lötscher <roland_loetscher@hotmail.com>
2023-09-28 18:14:33 +0200

    Split button: integrated opacity and thickness

--- splitbutton/main.js
+++ splitbutton/main.js
@@ -49,6 +49,12 @@ async function set_css_property(widget, property, value) {
   context.add_provider(provider, Gtk.STYLE_PROVIDER_PRIORITY_APPLICATION);
 }
 
+function hex_from_color_and_opacity(color, opacity) {
+  const alpha_decimal = Math.floor((opacity + 100) * 1.275);
+  const alpha = alpha_decimal.toString(16);
+  return `${color}${alpha}`;
+}
+
 const Marker = GObject.registerClass(
   {
     GTypeName: "Marker",
@@ -61,20 +67,28 @@ const Marker = GObject.registerClass(
       "button_red",
       "button_color_chooser",
       "scale_thickness",
+      "ad_thickness",
       "scale_opacity",
+      "ad_opacity",
       "im_linestyle",
       "im_drawingtype",
     ],
   },
   class Marker extends Gtk.Grid {
     // register signal handlers and other symbols
-    constructor(color, linestyle, drawingtype) {
+    constructor(color, marker_opacity, linestyle, thickness, drawingtype) {
       super();
       this.color = color;
-      this.linestyle = linestyle;
-      set_css_property(this._marker, "color", color);
-      this._im_linestyle.set_from_icon_name(`line-style-${linestyle}-symbolic`);
-      this._im_drawingtype.set_from_icon_name(`${drawingtype}-symbolic`);
+      this.marker_opacity = marker_opacity;
+      set_css_property(
+        this._marker,
+        "color",
+        hex_from_color_and_opacity(color, marker_opacity),
+      );
+      this._im_linestyle.set_from_icon_name(`line-style-${linestyle}`);
+      this._im_drawingtype.set_from_icon_name(`${drawingtype}`);
+      this._ad_opacity.set_value(marker_opacity);
+      this._ad_thickness.set_value(thickness);
       for (const [value, label] of Object.entries(marks_thickness)) {
         this._scale_thickness.add_mark(value, Gtk.PositionType.ABOVE, "");
       }
@@ -89,79 +103,109 @@ const Marker = GObject.registerClass(
     }
 
     on_color_button_clicked(_self) {
+      console.log(this.color);
       console.log(_self.name);
-      let color = "";
 
       switch (_self.name) {
         case "button_green":
-          color = "#8ff0a4";
+          this.color = "#8ff0a4";
           break;
         case "button_yellow":
-          color = "#e5a50a";
+          this.color = "#e5a50a";
           break;
         case "button_red":
-          color = "#f66151";
+          this.color = "#f66151";
           break;
       }
-      set_css_property(this._marker, "color", color).catch(console.error);
+      set_css_property(
+        this._marker,
+        "color",
+        hex_from_color_and_opacity(this.color, this.marker_opacity),
+      ).catch(console.error);
     }
 
     on_linestyle_button_clicked(_self) {
-      console.log(_self.name);
-
       switch (_self.name) {
         case "linestyle_plain":
-          this._im_linestyle.set_from_icon_name("line-style-plain-symbolic");
+          this._im_linestyle.set_from_icon_name("line-style-plain");
           break;
         case "linestyle_dash":
-          this._im_linestyle.set_from_icon_name("line-style-dash-symbolic");
+          this._im_linestyle.set_from_icon_name("line-style-dash");
           break;
         case "linestyle_dash_dot":
-          this._im_linestyle.set_from_icon_name("line-style-dash-dot-symbolic");
+          this._im_linestyle.set_from_icon_name("line-style-dash-dot");
           break;
         case "linestyle_dot":
-          this._im_linestyle.set_from_icon_name("line-style-dot-symbolic");
+          this._im_linestyle.set_from_icon_name("line-style-dot");
           break;
       }
     }
 
     on_drawingtype_button_clicked(_self) {
-      console.log(_self.name);
-
       switch (_self.name) {
         case "drawingtype_rect":
-          this._im_drawingtype.set_from_icon_name("rect-symbolic");
+          this._im_drawingtype.set_from_icon_name("rect");
           break;
         case "drawingtype_ellipse":
-          this._im_drawingtype.set_from_icon_name("ellipse-symbolic");
+          this._im_drawingtype.set_from_icon_name("ellipse");
           break;
         case "drawingtype_arrow":
-          this._im_drawingtype.set_from_icon_name("arrow-symbolic");
+          this._im_drawingtype.set_from_icon_name("arrow");
           break;
         case "drawingtype_doublearrow":
-          this._im_drawingtype.set_from_icon_name("double-arrow-symbolic");
+          this._im_drawingtype.set_from_icon_name("double-arrow");
           break;
         case "drawingtype_line":
-          this._im_drawingtype.set_from_icon_name("line-symbolic");
+          this._im_drawingtype.set_from_icon_name("line");
           break;
         case "drawingtype_coordinatesystem":
-          this._im_drawingtype.set_from_icon_name("coordinate-system-symbolic");
+          this._im_drawingtype.set_from_icon_name("coordinate-system");
           break;
         case "drawingtype_spline":
-          this._im_drawingtype.set_from_icon_name("spline-symbolic");
+          this._im_drawingtype.set_from_icon_name("spline");
           break;
         case "drawingtype_shaperecognizer":
-          this._im_drawingtype.set_from_icon_name("shape-recognizer-symbolic");
+          this._im_drawingtype.set_from_icon_name("shape-recognizer");
           break;
       }
+    }
+
+    on_thickness_changed(_self) {
+      this.thickness = _self.value;
+      set_css_property(
+        this._im_linestyle,
+        "-gtk-icon-transform",
+        `scale(${(this.thickness + 2.0) / 3.0})`,
+      );
+    }
+
+    on_opacity_changed(_self) {
+      this.marker_opacity = _self.value;
+      set_css_property(
+        this._marker,
+        "color",
+        hex_from_color_and_opacity(this.color, this.marker_opacity),
+      );
     }
   },
 );
 
 const box = new Gtk.Box({ spacing: 24 });
-const marker1 = new Marker("#8ff0a4", "dash-dot", "arrow");
-const marker2 = new Marker("#f66151", "plain", "ellipse");
-marker1._scale_opacity.set_value(10);
+const marker1 = new Marker(
+  "#8ff0a4", // color
+  50.0, // opacity
+  "dash-dot", // line style
+  Object.keys(marks_thickness)[1], // thickness
+  "arrow", // drawing type
+);
+const marker2 = new Marker(
+  "#f66151",
+  80.0,
+  "plain",
+  Object.keys(marks_thickness)[3],
+  "ellipse",
+);
+marker1._ad_opacity.set_value(50.0);
 box.append(marker1);
 box.append(marker2);
 
