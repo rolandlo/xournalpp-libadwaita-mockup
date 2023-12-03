4c01d714575786769b935f8bc0e6131469b69f0b
Roland Lötscher <roland_loetscher@hotmail.com>
2023-12-03 16:51:56 +0100

    Render sample pdf and replace stack switcher

--- src/util.py
+++ src/util.py
@@ -3,8 +3,8 @@ import os
 script_dir = os.path.dirname(os.path.realpath(__file__))
 
 
-def get_ui_path(ui_file):
-    return os.path.normpath(os.path.join(script_dir, "..", "ui", ui_file))
+def get_files_uri(file):
+    return "file://" + os.path.normpath(os.path.join(script_dir, "files", file))
 
 
 def get_resource_path(resource_file):
--- src/window.py
+++ src/window.py
@@ -18,9 +18,9 @@
 #
 # SPDX-License-Identifier: GPL-2.0-or-later
 
-from gi.repository import Adw, Gtk
+from gi.repository import Adw, Gtk, Poppler
 from .marker import Marker
-from .util import get_resource_path
+from .util import get_resource_path, get_files_uri
 
 
 @Gtk.Template(resource_path=get_resource_path("ui/window.ui"))
@@ -29,6 +29,7 @@ class MainWindow(Adw.ApplicationWindow):
 
     content_box = Gtk.Template.Child()
     drawing_area = Gtk.Template.Child()
+    stack = Gtk.Template.Child()
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
@@ -43,12 +44,34 @@ class MainWindow(Adw.ApplicationWindow):
         self.content_box.prepend(self.marker)
 
         self.drawing_area.set_draw_func(self.draw)
+        print(get_files_uri("sample.pdf"))
+        self.document = Poppler.Document.new_from_file(get_files_uri("sample.pdf"))
+        self.page = self.document.get_page(0)
 
     def draw(self, area, cr, width, height):
+        w, h = self.page.get_size()
         # Draw rectangle in drawing area
-        cr.rectangle(30, 10, width - 60, height - 20)
-        cr.set_source_rgba(0.9, 0.9, 0.9, 1)
+        f = 1.0
+        print(w, width, h, height)
+        if width < w or height < h:
+            f = min(width / w, height / h)
+        cr.rectangle(0, 0, w, h)
+        cr.set_source_rgba(0.9, 0.9, 0.9, 1)  # light gray
         cr.fill()
-        cr.rectangle(30, 10, width - 60, height - 20)
-        cr.set_source_rgba(1, 0, 0, 1)
+        cr.rectangle(0, 0, w, h)
+        cr.set_source_rgba(1, 0, 0, 1)  # red
         cr.stroke()
+        cr.scale(f, f)
+        self.page.render(cr)
+
+    @Gtk.Template.Callback()
+    def on_button_preview_clicked(self, button):
+        self.stack.set_visible_child_name("page_preview")
+
+    @Gtk.Template.Callback()
+    def on_button_workspace_clicked(self, button):
+        self.stack.set_visible_child_name("page_workspace")
+
+    @Gtk.Template.Callback()
+    def on_button_library_clicked(self, button):
+        self.stack.set_visible_child_name("page_library")
